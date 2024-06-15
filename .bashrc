8d45091ac04578fca06143852bd5021edbabeaea
user01 <user01@mex.fritz.box>
2024-06-15 22:52:38 +0200

    test

---
+++ .bashrc
@@ -0,0 +1,33 @@
+# Set the prompt color to red and make it bold
+PS1='\[\e[1;31m\]\u@\h \w \[\e[0m\]\$ '
+
+alias l="ls -lah --color=auto"
+alias brave="nohup brave-browser & disown"
+export PATH="$HOME/.cargo/bin:$PATH"
+alias shutdown="sudo shutdown -h now"
+alias chrome="nohup chromium & disown"
+alias writer="libreoffice --writer"
+alias calc="libreoffice --calc"
+alias impress="libreoffice --impress"
+alias draw="libreoffice --draw"
+alias base="libreoffice --base"
+
+
+# set history size
+export HISTSIZE=20000
+. "$HOME/.cargo/env"
+
+
+# Function to get the current git branch
+function parse_git_branch() {
+  git branch 2>/dev/null | grep '*' | sed 's/* //'
+}
+
+# Custom prompt to include git branch only if inside a git repository
+export PS1="\[\033[01;34m\]\u@\h \[\033[01;32m\]\W\[\033[01;33m\]\$(parse_git_branch | sed 's/^/ git:(/;s/$/)/')\[\033[00m\] \$ "
+
+# Start ssh-agent if it is not already running
+eval "$(ssh-agent -s)"
+
+# Add your SSH private key to the ssh-agent
+ssh-add ~/github/ssh/githubenjoyer1337/github_masterkey
---
+++ essential-bashscript.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Making .config and Moving config files
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/screenshots
+cp -fr /home/$username/dotconfig-debian/.config /home/$username/
+cp .bashrc /home/$username/.bashrc
+cp .vimrc /home/$username/.vimrc
+cp .tmux.conf /home/$username/.tmux.conf
+chown -R $username:$username /home/$username
+
+
+# Creating symbolic links for root user
+ln -sf /home/$username/.bashrc /root/.bashrc
+ln -sf /home/$username/.vimrc /root/.vimrc
+ln -sf /home/$username/.config /root/.config
+
+# Installing Essential Programs 
+apt install sudo xorg kitty wget curl tmux build-essential dos2unix exfat-fuse exfatprogs ntfs-3g alsa-utils pulseaudio pavucontrol net-tools nmap feh gdisk gimp maim slop xclip ripgrep vim vim-gtk3 lightdm i3 golang -y
+
+
+# for ss and pic viewing and editing
+# gimp maim slop
+
+# for formatting:
+# exfat-fuse exfatprogs
+# ntfs-3g
+# gdisk for switching partitioning scheme
+# 
+# for audio:
+# alsa-utils pulseaudio pavucontrol
