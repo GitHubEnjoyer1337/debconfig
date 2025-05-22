d01a8c125fc9c642fe57ac9478f102c272655569
mex <you@example.com>
2025-05-22 22:28:30 +0200

    added pipx

--- setup.sh
+++ setup.sh
@@ -116,7 +116,7 @@ apt install sudo xorg wget curl tmux build-essential dos2unix exfat-fuse exfatpr
 alsa-utils pulseaudio pavucontrol net-tools nmap feh gdisk gimp maim slop xclip ripgrep \
 zathura vim vim-gtk3 sddm i3 golang exiftool lshw rsync libreoffice redshift e2fsprogs \
 zsh pkg-config acl git dnsutils lxpolkit thunderbolt-tools bolt libssl-dev p7zip-full \
-ffmpeg chrony keepassxc btop plocate lm-sensors lsof pass python3-pip neofetch ranger \
+ffmpeg chrony keepassxc btop plocate lm-sensors lsof pass python3-pip pipx neofetch ranger \
 docker podman netcat-openbsd -y
 
 
