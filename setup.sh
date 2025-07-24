06f7e93223f988d44db5efd0de41be1872a04875
user01 <maxkellermayr@gmail.com>
2025-07-24 22:48:30 +0200

    added packs for c

--- setup.sh
+++ setup.sh
@@ -113,12 +113,14 @@ create_dir "/root/.local/share/nvim/site/pack/packer/start"
 create_dir "/root/.config/i3"
 
 # Install Essential Programs
-apt install sudo xorg wget curl tmux build-essential dos2unix exfat-fuse exfatprogs ntfs-3g \
-alsa-utils pulseaudio pavucontrol net-tools nmap feh gdisk gimp maim slop xclip ripgrep \
-zathura vim vim-gtk3 sddm i3 golang exiftool lshw rsync libreoffice redshift e2fsprogs \
-zsh pkg-config acl git dnsutils lxpolkit thunderbolt-tools bolt libssl-dev p7zip-full \
-ffmpeg chrony keepassxc btop plocate lm-sensors lsof pass python3-pip pipx neofetch ranger \
-docker podman netcat-openbsd alacritty whois -y
+apt install sudo xorg wget curl tmux build-essential dos2unix exfat-fuse \
+exfatprogs ntfs-3g alsa-utils pulseaudio pavucontrol net-tools nmap feh \
+gdisk gimp maim slop xclip ripgrep zathura vim vim-gtk3 sddm i3 golang \
+exiftool lshw rsync libreoffice redshift e2fsprogs zsh pkg-config acl \
+git dnsutils lxpolkit thunderbolt-tools bolt libssl-dev p7zip-full \
+ffmpeg chrony keepassxc btop plocate lm-sensors lsof pass python3-pip \
+pipx neofetch ranger docker podman netcat-openbsd alacritty whois \
+clang llvm clangd clang-format clang-tidy gdb cmake make -y
 
 
 # Install Oh My Zsh for root if not already installed
